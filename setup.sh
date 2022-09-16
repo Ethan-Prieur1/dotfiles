55193e8a865e3b33c0ca4297dce1963b00397960
Ethan-Prieur1 <99287118+Ethan-Prieur1@users.noreply.github.com>
2022-09-16 10:00:37 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,10 @@
 # sudo apt install -y git
 #
 # git clone https://github.com/Ethan-Prieur1/dotfiles.git
-# cd update and upgrade
+# cd ./dotfiles && sh ./setup.sh
+# update and upgrade
 
-Echo Update and Upgrading
+echo Update and Upgrading
 sudo apt-get update
 sudo apt-get full-upgrade -y
 
@@ -19,6 +20,46 @@ git config --global pull.rebase true
 git config --global fetch.prune true
 
 # Make Folders
-mkdir ICS4U
+mkdir ~/ICS4U
+mkdir ~/ICS4U/Unit1
+mkdir ~/ICS4U/Unit2
+mkdir ~/ICS4U/Unit3
+mkdir ~/ICS4U/Unit4
+mkdir ~/ICS4U/Assignments
+mkdir ~/ICS4U/Unit1/Unit1-01
+mkdir ~/ICS4U/Unit1/Unit1-02
+mkdir ~/ICS4U/Unit1/Unit1-03
+mkdir ~/ICS4U/Unit1/Unit1-04
+mkdir ~/ICS4U/Unit1/Unit1-05
+mkdir ~/ICS4U/Unit1/Unit1-06
+mkdir ~/ICS4U/Unit1/Unit1-07
+mkdir ~/ICS4U/Unit2/Unit2-01
+mkdir ~/ICS4U/Unit2/Unit2-02
+mkdir ~/ICS4U/Unit2/Unit2-03
+mkdir ~/ICS4U/Unit2/Unit2-04
+mkdir ~/ICS4U/Unit2/Unit2-05
+mkdir ~/ICS4U/Unit2/Unit2-06
+mkdir ~/ICS4U/Unit2/Unit2-07
+mkdir ~/ICS4U/Unit2/Unit2-08
+mkdir ~/ICS4U/Unit2/Unit2-09
+mkdir ~/ICS4U/Unit2/Unit2-10
+mkdir ~/ICS4U/Unit3/Unit3-01
+mkdir ~/ICS4U/Unit3/Unit3-02
+mkdir ~/ICS4U/Unit3/Unit3-03
+mkdir ~/ICS4U/Unit3/Unit3-04
+mkdir ~/ICS4U/Unit3/Unit3-05
+
+# Setup Github SSH keys
+echo "Press Return 3 Times"
+ssh-keygen -t ed25519 -C "ethan.prieur@mths.ca"
+ssh-add ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519.pub
+echo "Copy and Paste above key into github ssh keys"
+echo "once done press return"
+read blank
+echo "You will be asked if you want to continue, type 'yes' "
+echo "After that press anything"
+read blank
+ssh -T git@github.com
 
 echo Done.
